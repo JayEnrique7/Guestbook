9417f46822d0a90bd78934996475795c6819c0cb
Juan Enrique <jfernandez7.jf@gmail.com>
2016-12-19 02:31:14 +0100

    Update Register.java

--- src/com/loginServlet/Register.java
+++ src/com/loginServlet/Register.java
@@ -10,40 +10,52 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/**
+ * this class register new users and check out if the user inserte a name and a password with help from the boolean class
+ */
 
 @WebServlet("/registerServlet")
 public class Register extends HttpServlet{
-
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 
 		resp.setContentType("text/html");
 		PrintWriter out = resp.getWriter();
-
-
 		String name = req.getParameter("name");
 		String password = req.getParameter("password");
 
-		if(name == null || name.equals("") || name.equals(" ") || password == null || password.equals("") || password.equals(" ")){
-			req.getRequestDispatcher("register.jsp").include(req, resp);
-			out.println("<br><font color=red>" + "Please write correctly" + "</font>");
-			out.close();
+		if(name == null && password == null || name == null && password.equals("") || name == null && password.equals("") || name.equals("") && password == null ||
+				name.equals("") && password.equals("") || name.equals("") && password.equals(" ") || name.equals(" ") && password == null || 
+				name.equals(" ") && password.equals("") || name.equals(" ") && password.equals(" ")){
 
+			req.getRequestDispatcher("register.jsp").include(req, resp);
+			out.println("<br><h2><font color=red>" + "Come on dude! Be serious and write correctly." + "</font></h2>");
+			out.close();
 		}
-		
+
+		if(name == null || name.equals("") || name == (" ")){
+
+			req.getRequestDispatcher("register.jsp").include(req, resp);
+			out.println("<br><h2><font color=red>" + "Are you crazy!?, No name!? come on dude insert your name..." + "</font></h2>");
+			out.close();
+		}
+
+		if(password == null || password.equals("") || password.equals(" ")){
+			req.getRequestDispatcher("register.jsp").include(req, resp);
+			out.println("<br><h2><font color=red>" + "Are you crazy!?, No password!? come on dude insert your password..." + "</font></h2>");
+			out.close();
+		}
+
 		else{
 			req.getRequestDispatcher("register.jsp").include(req, resp);
 			registerLogin(name, password);
-			out.println("<br><font color=blue>" + "Registration successful!" + "</font>");
-
+			out.println("<br><h2><font color=blue>" + "OMG! You're a legend! Registration successful!<br>Go to login page." + "</font></h2>");
 		}
-		
+
 		out.close();
 	}
-
-
 
 	public boolean registerLogin(String email, String password){
 		String sql = "INSERT INTO user(email, password) VALUES (?, ?)";
@@ -54,12 +66,10 @@ public class Register extends HttpServlet{
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			stmt.setString(1, email);
 			stmt.setString(2, password);
-
 			stmt.executeUpdate();
 
 		}
 		catch(SQLException e) {
-
 			e.printStackTrace();
 			return false;
 		}
@@ -69,13 +79,6 @@ public class Register extends HttpServlet{
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-
 		doGet(req, resp);
-
 	}
-
-
-
-
 }
-
